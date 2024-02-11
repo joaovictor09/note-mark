3dd6730c205ea0a5d383fb427f2a9b54c9ac2f6e
João Victor da Silva <joaovictordasilva0911@gmail.com>
2024-02-11 06:20:20 -0300

    feat: adding the editor

--- src/renderer/src/app.tsx
+++ src/renderer/src/app.tsx
@@ -2,6 +2,9 @@ import '@/assets/index.css'
 import { Content, RooLayout, Sidebar } from '@/components'
 import { ActionButtonsRow } from './components/action-buttons-row'
 import { DraggableTopBar } from './components/draggable-top-bar'
+import { FloatingNoteTitle } from './components/floating-note-title'
+import { MarkdownEditor } from './components/markdown-editor'
+import { NotePreviewList } from './components/note-preview-list'
 
 export function App() {
   return (
@@ -10,8 +13,12 @@ export function App() {
       <RooLayout>
         <Sidebar className="p-2">
           <ActionButtonsRow className="flex justify-between mt-1" />
+          <NotePreviewList className="mt-3 space-y-1" />
         </Sidebar>
-        <Content className="">Content</Content>
+        <Content className="">
+          <FloatingNoteTitle  className='pt-2'/>
+          <MarkdownEditor />
+        </Content>
       </RooLayout>
     </>
   )
